6bf1941a818ed1bd8ff6a2048b01a82a004ac417
Jonathan <sir15004@byui.edu>
2020-01-15 21:48:24 -0800

    made practice modal appear with translated english to morse

--- src/components/DrillCard.tsx
+++ src/components/DrillCard.tsx
@@ -1,18 +1,27 @@
-import { IonCard, IonText } from "@ionic/react";
-import React from "react";
+import { IonCard, IonText, IonPopover } from "@ionic/react";
+import React, { useState } from "react";
+import Dictionary from './../util/dictionary';
 import './DrillCard.scss';
 
 interface DrillCardProps {
     title: string
 }
 
-
-
 const DrillCard: React.FunctionComponent<DrillCardProps> = ({ title }) => {
+    const [showPopover, setShowPopover] = useState(false);
+
     return (
-        <IonCard className="drillCard">
+        <>
+        <IonPopover
+                isOpen={showPopover}
+                onDidDismiss={e => setShowPopover(false)}
+            >
+            <IonText className="popupText">{Dictionary.translate(title)}</IonText>
+        </IonPopover>
+        <IonCard className="drillCard" onClick={e => setShowPopover(true)}>
             <IonText>{ title }</IonText>
         </IonCard>
+        </>
     );
 }
 
